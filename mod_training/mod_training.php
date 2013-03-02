60244c9bff8b9ee386f2425aff98f15152ec8acf
triley63 <rileyta63@yahoo.com>
2013-03-01 21:40:35 -0600

    Updated the module (mod_training)

--- mod_training/mod_training.php
+++ mod_training/mod_training.php
@@ -1,33 +1,72 @@
 <?php
 
+// <editor-fold defaultstate="collapsed" desc="Header Information">
+/**
+ * This module is used with the Joomla framework and the
+ * @see com_training component. This will provide a dynamic
+ * menu for the training and examination component.
+ * 
+ * @copyright Copyright 2013 (c), All Rights Reserved
+ * 
+ * @category JPlugins
+ * @package Training Module Menu (mod_training)
+ * 
+ * @license http://www.opensource.org/licenses/BSD-3-Clause
+ * @version 1.0.0
+ * @since 0.0.01
+ * 
+ * @author Trent Riley <triley63@vpssolutions.com, trentr@threerivershospital>
+ * 
+ */
+// </editor-fold>
+
 defined ( '_JEXEC' ) or die;
 
+//get the current user
+$user = JFactory::getUser();
+
+// <editor-fold defaultstate="collapsed" desc="Database Call">
 $db = JFactory::getDBO();
-
 $query = $db->getQuery(true);
-$query->select('trainingID, name')
-        ->from('#__Training_Training')
-        ->where ('active = 1');
 
+$query->select("t.trainingID, t.name, t.assignedOnly")
+        ->from("#__Training_Training AS t")
+        ->leftJoin("#__Training_UserTraining AS ut ON t.trainingID = ut.trainingID")
+        ->leftJoin("#__users AS u ON ut.userID = u.id")
+        ->where("t.active = 1 OR t.assignedOnly = 0");
+        
 $db->setQuery($query);
 $rows = $db->loadObjectList();
+// </editor-fold>
 
-?>
+// <editor-fold defaultstate="collapsed" desc="Set Menu Items">
+$assigned = '<ul><h4>Assigned</h4>';
+$unassigned = '<hr><h4>Additional Training</h4><ul>';
 
-<?php
-    if (empty($rows))
+foreach ($rows as $row):
+    if ($row->assignedOnly == "1")
     {
-        echo "Topics will go here";
+        $url = 'index.php?option=com_training&view=course&id=' . $row->trainingID;
+        $assigned .= '<li><a href=' . JRoute::_($url) . '>' . $row->name . '</a>';
     }
     else
     {
-?>
-    <ul>
-    <?php foreach ($rows as $row): ?>
-        <li>
-            <?php $url = 'index.php?option=com_training&view=course&id=' . $row->trainingID; ?>
-            <a href="<?php echo JRoute::_($url); ?>"><?php echo $row->name; ?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-<?php }?>
+        $url = 'index.php?option=com_training&view=course&id=' . $row->trainingID;
+        $unassigned .= '<li><a href=' . JRoute::_($url) . '>' . $row->name . '</a>';        
+    }
+endforeach;
+
+$assigned .= '</ul>';
+$unassigned .= '</ul>';
+// </editor-fold>
+
+if ($user->guest)
+{
+    echo 'You must login to see the assigned training';
+}
+else
+{
+    echo $assigned;
+}
+
+echo $unassigned;
